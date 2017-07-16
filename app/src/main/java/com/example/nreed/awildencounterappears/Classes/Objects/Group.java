1ef76a7a9fc1dd5ced099f84d4a2345e5829f674
Nicole Reed <nicole.reed88@gmail.com>
2017-07-16 15:06:22 -0400

    Grouplist

--- app/src/main/java/com/example/nreed/awildencounterappears/Classes/Objects/Group.java
+++ app/src/main/java/com/example/nreed/awildencounterappears/Classes/Objects/Group.java
@@ -1,10 +1,13 @@
 package com.example.nreed.awildencounterappears.Classes.Objects;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Niki on 7/16/2017.
  */
 
-public class Group {
+public class Group implements Parcelable{
     private int groupId;
     private String groupName;
 
@@ -23,4 +26,33 @@ public class Group {
     public void setGroupName(String groupName) {
         this.groupName = groupName;
     }
+    public Group(){
+
+    }
+    public Group(Parcel in){
+        setGroupId(in.readInt());
+        setGroupName(in.readString());
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(getGroupId());
+        parcel.writeString(getGroupName());
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Group createFromParcel(Parcel in) {
+            return new Group(in);
+        }
+
+        public Group[] newArray(int size) {
+            return new Group[size];
+        }
+    };
 }
