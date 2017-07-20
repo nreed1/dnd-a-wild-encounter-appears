99f3d48877e603363f1658376e755ecbc440eb0a
Nicole Reed <nicole.reed88@gmail.com>
2017-07-20 08:32:58 -0400

    Settings Fragment Base

--- app/src/main/java/com/example/nreed/awildencounterappears/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/nreed/awildencounterappears/Fragments/SettingsFragment.java
@@ -3,8 +3,7 @@ package com.example.nreed.awildencounterappears.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,7 @@ import com.example.nreed.awildencounterappears.R;
  * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends Fragment  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
