2676da66f59ee9b081381779d42e7990d6bf002d
Nicole Reed <nreed@geekhive.com>
2017-08-30 10:43:07 -0400

    Settings Table

---
+++ app/src/main/java/com/example/nreed/awildencounterappears/Classes/DataAdapters/Database/SettingsTable.java
@@ -0,0 +1,10 @@
+package com.example.nreed.awildencounterappears.Classes.DataAdapters.Database;
+
+/**
+ * Created by nreed on 8/1/2017.
+ */
+
+public class SettingsTable {
+    private static String TABLE_SETTINGS = "settings";
+    private static String COLUMNN_SOURCES = "allowedsources";
+}
